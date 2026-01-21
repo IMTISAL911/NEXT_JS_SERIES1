faaf61143c0ac4cb68974bca3334d85a4104f293
IMTISAL HUSSAIN <imtisalhussan@gmail.com>
2026-01-21 22:47:05 +0500

    6th series nextJs

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,13 @@
-
-export default function Contact () {
-    return(
-        <div className=" flex justify-center mt-4">
-            <h1>Contact PAGE</h1>
+"use client";
+export default function Contact() {
+    return (
+        <div className=" h-screen bg-purple-500">
+            <div className="flex justify-end gap-[2%]  mr-[5%] flex-wrap">
+                <a className="text-white hover:text-gray-200 transition-colors duration-200 font-sans font-bold text-lg mt-[2%]" href="#home">Home</a>
+                <a className="text-white hover:text-gray-200 transition-colors duration-200 font-sans font-bold text-lg mt-[2%]" href="#about">About</a>
+                <a className="text-white hover:text-gray-200 transition-colors duration-200 font-sans font-bold text-lg mt-[2%]" href="#contact">Contact</a>
+                <a className="text-white hover:text-gray-200 transition-colors duration-200 font-sans font-bold text-lg mt-[2%]" href="#services">Services</a>
+            </div>
         </div>
     )
 }
