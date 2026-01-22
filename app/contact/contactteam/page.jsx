72b51b0b6d820d7ee9ed2e076981850f2ea2a9b9
IMTISAL HUSSAIN <imtisalhussan@gmail.com>
2026-01-22 18:53:20 +0500

    9th series of Nextjs

---
+++ app/contact/contactteam/page.jsx
@@ -0,0 +1,14 @@
+
+import Link from "next/link"
+
+
+export default function ContactTeam () {
+    return(
+        <div>
+<h1> Contact Team</h1>
+
+<Link href="/">Go to home</Link>
+
+        </div>
+    )
+}
